6adb8db6aca27b5a7cf9be7d1580cdb9b48bb6dd
SilentSpectre-arch <amirootrpr234@gmail.com>
2026-08-12 20:56:25 +0330

    feat: add port scanner

---
+++ port-scanner.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+
+import socket
+
+target=input("Enter Target: ")
+
+for port in range(1,1025):
+    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    result=sock.connect_ex((target,port))
+
+    if result == 0:
+        print(f"Port: {port} is open")
+
+    
+    sock.close()
