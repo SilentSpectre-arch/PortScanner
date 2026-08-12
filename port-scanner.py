edd02efb7a2fe6889ba05376ba341716fcaf370d
SilentSpectre-arch <amirootrpr234@gmail.com>
2026-08-12 21:32:57 +0330

    refactor port scan

--- port-scanner.py
+++ port-scanner.py
@@ -5,14 +5,24 @@ import socket
 def scan_port(target,port):
     sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(2)
-    result=sock.connect_ex((target,port))
 
-    if result == 0:
-        print(f"Port: {port} is open")
-    sock.close()
+    try:
+        result=sock.connect_ex((target,port))
 
-target= input("Enter target: ")
+        if result == 0:
+            return True
+
+        return False
+
+    finally:
+        sock.close()
+
+target=input("Enter Target: ")
+
+open_ports=[]
 
 for port in range(1,1025):
-    scan_port(target,port)
+    if scan_port(target,port):
+        open_ports.append(port)
 
+print(open_ports)
