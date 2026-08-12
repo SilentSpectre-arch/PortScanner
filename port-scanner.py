2a5f4ef9224c9d9e1598fdbb6032b73c39b3d8c9
SilentSpectre-arch <amirootrpr234@gmail.com>
2026-08-12 22:15:07 +0330

    refactor scanner and color variable definition

--- port-scanner.py
+++ port-scanner.py
@@ -1,28 +1,48 @@
 #!/bin/env python3
 
 import socket
+from concurrent.futures import ThreadPoolExecutor
+import sys
+import time
+
+#Colors
+
+PINK="\033[95m"
+GREEN="\033[92m"
+RED="\033[91m"
+CYAN="\033[96m"
+YELLOW="\033[93m"
+RESET="\033[0m"
 
 def scan_port(target,port):
     sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(2)
+    sock.settimeout(0.5)
 
     try:
-        result=sock.connect_ex((target,port))
+        result= sock.connect_ex((target,port))
 
         if result == 0:
-            return True
+            return port
 
-        return False
+        return None
 
     finally:
         sock.close()
 
-target=input("Enter Target: ")
+def main():
+    target=input("Enter Target: ")
+    p_s= int(input("Start Port: "))
+    p_e= int(input("End Port: "))
+    ports=range(p_s,p_e)
 
-open_ports=[]
+    with ThreadPoolExecutor(max_workers=100) as exec:
+        result = exec.map(
+            lambda port: scan_port(target,port),ports
+        )
 
-for port in range(1,1025):
-    if scan_port(target,port):
-        open_ports.append(port)
+        for result in result:
+            if result:
+                print(f"Port {result} is open")
 
-print(open_ports)
+if __name__ == "__main__":
+    main()
